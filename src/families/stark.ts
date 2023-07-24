3336091840568ad1a43bcb674c12ae60e5831262
TomOwen93 <mr.tsowen@gmail.com>
2023-07-24 10:29:36 +0100

    initial commit

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -1,6 +1,31 @@
 import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
+  const rickardsChildrenNames = ["Benjen", "Lyanna", "Brandon", "Eddard"];
+  const eddardsChildrenNames = [
+    "Rickon",
+    "Bran",
+    "Arya",
+    "Sansa",
+    "Robb",
+    "Jon",
+  ];
 
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+  const childObject = (child: string): Person => {
+    return { name: child, children: [] };
+  };
+
+  const eddardsChildren: Person[] = eddardsChildrenNames.map((child) =>
+    childObject(child),
+  );
+
+  const rickardsChildren: Person[] = rickardsChildrenNames.map((child) =>
+    childObject(child),
+  );
+
+  rickardsChildren[3].children = eddardsChildren;
+
+  return { name: "Rickard", children: rickardsChildren };
 }
+
+console.log(createStarkTree());
